edfd9c98e20c8de8bbb24e8d0cc9a5c31bbb3169
Van Thong <thonglevan131@gmail.com>
2020-08-03 20:24:32 +0700

    implemented composition design pattern with interface, and wrote static class to prevent verbose function call

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
 
-const reader = new MatchReader('football.csv');
-reader.read();
-let manUnitedWins = 0;
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
 
-for (let match of reader.data) {
-	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-		manUnitedWins++;
-	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-		manUnitedWins++;
-	}
-}
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+summary.buildAndPrintReport(matchReader.matches);
